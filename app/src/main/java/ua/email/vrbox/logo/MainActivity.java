e79fa03d29ae29fdc58bf8cda17e7fe81c0cb49f
vrbook <vrb-box@email.ua>
2017-09-25 11:48:32 +0300

    Первый commit.

---
+++ app/src/main/java/ua/email/vrbox/logo/MainActivity.java
@@ -0,0 +1,56 @@
+package ua.email.vrbox.logo;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+
+        runSplash();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_splash) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void runSplash(){
+        SplashFragment splashFragment = new SplashFragment();
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, splashFragment)
+                .addToBackStack(null)
+                .commit();
+
+    }
+}
